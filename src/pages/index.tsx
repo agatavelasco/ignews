4afb78461df9c433c5b37668791bfc9e735c33c0
Ágata Velasco Penze <avpenze@gmail.com>
2021-06-03 21:26:41 -0400

    Configurando static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -33,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Iy5YbFz6lCn2My8d89T1p4v')
 
   const product = {
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
    props: {
      product
-   }
+   },
+   revalidate: 60 * 60 * 24, // 24h hours
   }
 }
